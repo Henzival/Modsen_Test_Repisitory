677fa3386e34f92c296af57badafba0ce29b6da0
Henzival <oleg78042869@gmail.com>
2023-06-07 22:16:17 +0300

    added 2 dropdown buttons, got rid of the importants in the css file, modified the searching panel

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,18 +1,48 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const App = function() {
   return (
-    <section class="header">
-      <div class="navbar navbar-dark bg-dark">
-        <div class="navbar_text">
-          <h1 class="greeting">
+    <section className="header">
+      <div className="navbar navbar-dark bg-dark">
+        <div className="navbar_text">
+          <h1 className="greeting">
             Search for books
           </h1>
         </div>
-        <form action="#" id="navbar-search" class="navbar_search" novalidate="novalidate">
-          <input type="search" name="search" class="input navbar_input" placeholder="Enter your book name here..." />
+        <form action="#" id="navbar-search" className="navbar_search">
+          <input type="search" name="search" className="input navbar_input" placeholder="Enter your book name here..." />
+          <span className="input-group-text border-0" id="search-addon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg>
+          </span>
         </form>
+        <div className='filtration'>
+          <span className='categories-text'>Categories</span>
+          <div class="dropdown dropdown-1">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown button
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+          <span className='sorting-text'>Sorting by</span>
+          <div class="dropdown dropdown-2">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown button
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div> 
       </div>
     </section>
   );
