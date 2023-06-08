862d186e16fc4e9af8570cbb49f73df85f90cb0b
Henzival <oleg78042869@gmail.com>
2023-06-08 22:57:16 +0300

    fixed a few bugs in the navbar section, added a new file with the new main section

---
+++ practice/src/Main.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const Main = function() {
+    return (
+        <section className="main">
+            <h1 className="results">Found x results</h1>
+
+        </section>
+    );
+}
+
+export default Main;
